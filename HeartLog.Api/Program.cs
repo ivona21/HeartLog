812c191a726f1bbfe074f7fdf96d8b66b90178bc
ivona21 <ivonamalekovic@hotmail.com>
2025-11-23 14:18:29 +0100

    additional cors domain for replit app

--- HeartLog.Api/Program.cs
+++ HeartLog.Api/Program.cs
@@ -86,11 +86,12 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowV0Frontend", policy =>
     {
         policy.WithOrigins(
-                "http://localhost:5173",      // your local frontend dev
-                "http://localhost:5001",
+                "http://localhost:5173",      // localhost 01
+                "http://localhost:5001", // localhost 02
                 "https://v0-heart-log-calm-fee5obmca-ivonas-projects-17db0703.vercel.app", // deployed V0 frontend,
-                "https://heart-log-calm.vercel.app",
-                "https://replit.com/@coffeebreak5551/HeartLogCalm"
+                "https://heart-log-calm.vercel.app", // deployed on v0
+                "https://71eb8564-b79f-4920-af09-9cd6317e6a88-00-1cgns8l1wsjzo.picard.replit.dev", // replit 01
+                "https://replit.com/@coffeebreak5551/HeartLogCalm" // replit 02
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
